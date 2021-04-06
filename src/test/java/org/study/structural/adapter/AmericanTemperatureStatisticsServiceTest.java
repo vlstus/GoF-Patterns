f23025386e403998d5361d30a7dc28108da8892c
Vladyslav Stusiak <vlstus377@gmail.com>
2021-04-06 15:44:05 +0200

    AmericanTemperatureStatisticsServiceTest created

---
+++ src/test/java/org/study/structural/adapter/AmericanTemperatureStatisticsServiceTest.java
@@ -0,0 +1,22 @@
+package org.study.structural.adapter;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.study.structural.adapter.client.TemperatureStatisticsService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AmericanTemperatureStatisticsServiceTest {
+
+    TemperatureStatisticsService service;
+
+    @BeforeAll
+    void setUp() {
+
+    }
+
+    @Test
+    void getAverageTemperatureBetween() {
+    }
+
+}
